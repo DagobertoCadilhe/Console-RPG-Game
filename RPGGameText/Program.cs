a4d758ac23ff62fae2db3c084e4fb4c0f47a2a39
DagobertoCadilhe <dagoberto.ccadilhe@gmail.com>
2025-02-13 07:53:23 -0300

    Adicionar arquivos de projeto.

---
+++ RPGGameText/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Player
+    {
+        private double playerHealth { get; set; }
+        private int playerXp { get; set; }
+        private int playterAttackDamage { get; set; }
+        private int playerDefence { get; set; }
+
+        public Player(double playerHealth, int playerXp, int playterAttackDamage, int playerDefence)
+        {
+
+        }
+
+
+    }
+}
